54cc81414341667cf8b7714f478cd50b90911288
Ratchadaporn Thongkhaw <pond7pond8@gmail.com>
2019-07-14 11:51:57 +0700

    MYapp4

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/play/:num1/:num2', function(req, res, next) {
+  //console.log(req.param)
+  var num1 = req.param.num1;
+  var num2 = req.param.num2;
+  var sum = parseInt(num1) + parseInt(num2);
+  res.send("ผลรวมของตัวเลข 2 จำนวนคือ " + sum);
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index',);
 });
 
 router.get('/create', function(req, res, next) {
-  res.render('index',);
+  res.render('create',);
 });
 
 router.get('/lonin', function(req, res, next) {
-  res.render('index', );
+  res.render('lonin', );
 });
 
 
