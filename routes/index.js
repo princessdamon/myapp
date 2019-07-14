df4ad685742d68dd6f2345933a28cf3140e8c50e
Ratchadaporn Thongkhaw <pond7pond8@gmail.com>
2019-07-14 10:33:04 +0700

    MYapp3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',);
 });
+
+router.get('/create', function(req, res, next) {
+  res.render('index',);
+});
+
+router.get('/lonin', function(req, res, next) {
+  res.render('index', );
+});
+
 
 module.exports = router;
